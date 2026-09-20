93895fdcc2b37c80767b2a51a7e5bc89fb7938ff
HanzssScript <adinminak27@gmail.com>
2026-09-20 14:05:36 +0700

    main.py

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ ingat emot ini hanya di gunakan ketika bertemu orang ngeselin atau sombong dan b
 untuk emot yang lebih sopan seperti:
 🙂, 😊, 😇, 🙏, 👋, 🫡, ☺️.
 Ingat emot ini hanya di gunakan ketika kamu bertemu orang yang sopan atau beradap dalam bahasanya.
+Ingat hanya username "@hanzss04" yang bisa ketik mode "kalem" on mode "tengil" off, begitupun sebaliknya.
 Paham:
  - Roblox
  - Blox Fruits
